3a4c5c9f78c726117c730f4ab9c37bcbeec909e0
Elizer Dolorosa <zykisriel0434@gmail.com>
2023-01-28 20:52:56 +0800

    Added image with unblurring

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import React, { useState } from 'react';
 
+{/*comment!!*/}
 export default function App() {
+  const handleOnPress = () => {
+    for(let i = blurAmount; i >= 0; i -= 0.01){
+      setTimeout(() => {  setBlur(blurAmount => blurAmount - 0.01); }, 5);
+    }{/*console.log("Kobe is pressed!");*/}
+  }
+  
+  const [blurAmount, setBlur] = useState(10);
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Kobe!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handleOnPress}>Kobe!</Text>
+      <Image
+        blurRadius={blurAmount}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/seed/picsum/200/300"}}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
